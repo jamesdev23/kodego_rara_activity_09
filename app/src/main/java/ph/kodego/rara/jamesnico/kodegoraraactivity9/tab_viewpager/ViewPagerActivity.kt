c50c482c1d714661a26e9cbf56835f8314cf7671
James Nico Rara <113577747+jamesdev23@users.noreply.github.com>
2023-02-24 18:16:48 +0800

    fixed ui design fixed image icons added double backpress to logout

--- app/src/main/java/ph/kodego/rara/jamesnico/kodegoraraactivity9/tab_viewpager/ViewPagerActivity.kt
+++ app/src/main/java/ph/kodego/rara/jamesnico/kodegoraraactivity9/tab_viewpager/ViewPagerActivity.kt
@@ -1,15 +1,21 @@
 package ph.kodego.rara.jamesnico.kodegoraraactivity9.tab_viewpager
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.SurfaceControl
+import android.widget.Toast
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
+import ph.kodego.rara.jamesnico.kodegoraraactivity9.LoginActivity
 import ph.kodego.rara.jamesnico.kodegoraraactivity9.adapter.FragmentAdapter
 import ph.kodego.rara.jamesnico.kodegoraraactivity9.databinding.ActivityViewPagerBinding
 
 class ViewPagerActivity : AppCompatActivity() {
     private lateinit var binding: ActivityViewPagerBinding
+    private var doubleBackToExitPressedOnce = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,11 +47,28 @@ class ViewPagerActivity : AppCompatActivity() {
     }
 
     override fun onBackPressed() {
-        if(binding.viewPager2.currentItem == 0) {
-            super.onBackPressed()
-        }else{
-            binding.viewPager2.currentItem = binding.viewPager2.currentItem - 1
+
+        if (doubleBackToExitPressedOnce) {
+            val backToLogin = Intent(this, LoginActivity::class.java)
+            startActivity(backToLogin)
+            Toast.makeText(applicationContext, "Logged out", Toast.LENGTH_SHORT).show()
+            finish()
+        } else {
+            Toast.makeText(applicationContext, "Press back again to logout.", Toast.LENGTH_SHORT).show()
         }
+
+        this.doubleBackToExitPressedOnce = true
+        Handler(Looper.getMainLooper())
+            .postDelayed(Runnable { doubleBackToExitPressedOnce = false }
+                , 2000)
+
+
+
+    //        if(binding.viewPager2.currentItem == 0) {
+    //            super.onBackPressed()
+    //        }else {
+    //            binding.viewPager2.currentItem = binding.viewPager2.currentItem - 1
+    //        }
     }
 
 
